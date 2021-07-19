97771138ced4d645dd941f4a9175d5210624f1a4
Kirill Mokevnin <mokevnin@gmail.com>
2021-07-19 14:55:56 +0400

    fix tests
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- __tests__/app.test.jsx
+++ __tests__/app.test.jsx
@@ -2,7 +2,7 @@ import '@testing-library/jest-dom';
 
 import { describe, it, expect } from '@jest/globals';
 import { render, screen } from '@testing-library/react';
-import App from '../pages/index.js';
+import App from '../pages/index.jsx';
 
 describe('App', () => {
   it('renders without crashing', () => {
