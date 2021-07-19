b497364a8e58a98aba17e2c42a0648e718499f33
Kirill Mokevnin <mokevnin@gmail.com>
2021-07-19 17:11:43 +0400

    fix component
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

---
+++ pages/professions/index.jsx
@@ -0,0 +1,11 @@
+// import Head from 'next/head';
+// import Image from 'next/image';
+
+const ProfessionsHome = () => {
+  const greeting = 'hay';
+  return (
+    <h1>{greeting}</h1>
+  );
+};
+
+export default ProfessionsHome;
