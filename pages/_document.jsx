bfe356cc88038e522e9edeea3cffaec8c43ef006
Roman Makarov <53420091+frontstall@users.noreply.github.com>
2021-07-22 18:49:41 +0300

    add gtm script (#20)

---
+++ pages/_document.jsx
@@ -0,0 +1,41 @@
+// @ts-check
+
+import React from 'react';
+import Document, {
+  Html, Head, Main, NextScript,
+} from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {process.env.NODE_ENV === 'production'
+          && (
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer', "${process.env.NEXT_PUBLIC_GTM_ID}");`,
+            }}
+          />
+          )}
+        </Head>
+        <body>
+          {process.env.NODE_ENV === 'production'
+          && (
+          <noscript dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id="${process.env.NEXT_PUBLIC_GTM_ID}""
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+          />
+          )}
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
