774c36941c5eae32282143f8c164121f14d985f2
Kirill Mokevnin <mokevnin@gmail.com>
2021-07-22 14:26:44 +0400

    fix deps
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- lint-staged.config.js
+++
@@ -1,6 +0,0 @@
-const { CI } = process.env;
-
-module.exports = {
-  '*.{js,jsx}': (filenames) => `npm run lint . ${filenames.join(' ')}${CI ? ' --fix' : ''}`,
-  'package.json': 'npmPkgJsonLint',
-};
--- npmpackagejsonlint.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: 'npm-package-json-lint-config-default',
-  rules: {
-    'prefer-alphabetical-dependencies': 'error',
-    'prefer-alphabetical-devDependencies': 'error',
-  },
-};
