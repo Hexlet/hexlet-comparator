e6525da3d2985c6d163df700ca11d71a953b7aae
Vasiliy Boldurean <64642760+boldurean@users.noreply.github.com>
2021-07-28 07:05:50 +0100

    Adding "Schools" link to Navbar (#35)
    * add: geekbrains shools info
    
    * add hexlet professions info
    
    * fix: assetsRoutes.js, hexlet screenshots/ schools info.
    
    * fix: assetsRoutes.js, hexlet screenshots/ schools info.
    
    * add: HTML Academy info
    
    * Add: Schools link to navbar / add locales
    
    * Add: about page + footer

--- components/layouts/BaseLayout.jsx
+++ components/layouts/BaseLayout.jsx
@@ -3,7 +3,9 @@ import routes from 'lib/routes.js';
 import { useTranslation } from 'next-i18next';
 import Head from 'next/head';
 import Link from 'next/link';
-import { Container, Navbar, Nav } from 'react-bootstrap';
+import {
+  Container, Navbar, Nav,
+} from 'react-bootstrap';
 // import Image from 'next/image';
 
 const CustomNavbar = () => {
@@ -15,6 +17,9 @@ const CustomNavbar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
+            <Link href={routes.schoolsPath()} passHref>
+              <Nav.Link>{t('navbar.schools')}</Nav.Link>
+            </Link>
             <Link href={routes.professionsPath()} passHref>
               <Nav.Link>{t('navbar.professions')}</Nav.Link>
             </Link>
@@ -24,6 +29,60 @@ const CustomNavbar = () => {
     </Navbar>
   );
 };
+
+const CustomFooter = () => (
+  <footer className="bg-light border-top mt-auto py-5">
+    <div className="container-xl">
+      <div className="row justify-content-lg-around">
+        <div className="col-sm-6 col-md-4 col-lg-auto">
+          <div className="fs-4 mb-2">Hexlet ©</div>
+          <ul className="list-unstyled">
+            <li>
+              <Link className="link-dark" href={routes.aboutPath()}>О проекте</Link>
+            </li>
+            <li>
+              <a className="link-dark" target="_blank" rel="noreferrer" href="https://github.com/Hexlet/hexlet-comparator">Исходный код</a>
+            </li>
+            <li>
+              <a className="link-dark" target="_blank" rel="noopener noreferrer" href="https://slack-ru.hexlet.io/">Slack #hexlet-volunteers</a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-sm-6 col-md-4 col-lg-auto">
+          <div className="fs-4 mb-2">Помощь</div>
+          <ul className="list-unstyled">
+            <li>
+              <a target="_blank" rel="noopener noreferrer" className="link-dark" href="https://ru.hexlet.io/blog/categories/success">Истории успеха</a>
+            </li>
+            <li>
+              <a className="link-dark" target="_blank" rel="noopener noreferrer" href="https://github.com/Hexlet/ru-test-assignments">Тестовые задания</a>
+            </li>
+            <li>
+              <a className="link-dark" target="_blank" rel="noopener noreferrer" href="https://ru.hexlet.io/courses/employment">Курс по трудоустройству</a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-sm-6 col-md-4 col-lg-auto">
+          <div className="fs-4 mb-2">Дополнительно</div>
+          <ul className="list-unstyled">
+            <li>
+              <a target="_blank" rel="noopener noreferrer" className="link-dark" href="https://ru.code-basics.com/">Основы программирования</a>
+            </li>
+            <li>
+              <a target="_blank" rel="noopener noreferrer" className="link-dark" href="https://codebattle.hexlet.io/">КодБатл (Игра для прокачки)</a>
+            </li>
+            <li>
+              <a target="_blank" rel="noopener noreferrer" className="link-dark" href="https://guides.hexlet.io/">Гайды для программистов</a>
+            </li>
+            <li>
+              <a target="_blank" rel="noopener noreferrer" className="link-dark" href="https://ru.hexlet.io/pages/recommended-books">Рекомендуемые книги</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+);
 
 const BaseLayout = (props) => {
   const { children } = props;
@@ -39,11 +98,7 @@ const BaseLayout = (props) => {
       <main className="container mt-5">
         {children}
       </main>
-      <footer className="container pt-4 my-md-5 pt-md-5 border-top">
-        <ul className="list-unstyled text-small">
-          <li><a target="_blank" rel="noreferrer" href="https://github.com/Hexlet/hexlet-comparator">Исходный код</a></li>
-        </ul>
-      </footer>
+      <CustomFooter />
     </>
   );
 };
---
+++ pages/about.jsx
@@ -0,0 +1,32 @@
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import Head from 'next/head.js';
+import { Container, Row } from 'react-bootstrap';
+import BaseLayout from '../components/layouts/BaseLayout.jsx';
+
+const About = () => (
+  <BaseLayout>
+    <Head>
+      <title>О проекте</title>
+    </Head>
+    <Container>
+      <Row>
+        <div className="m-2">
+          <h1>О проекте</h1>
+          <p>
+            Hexlet Comparator - это проект с открытым исходным кодом, созданный и поддерживаемый сообществом Hexlet.
+            Присоединяйтесь к сообществу, публикуйте резюме и оставляйте рекомендации другим участникам. Расскажите о сайте коллегам и друзьям!
+            Исходный код платформы Hexlet Comparator доступен на GitHub. Вы можете поучаствовать в процессе разработки.
+          </p>
+        </div>
+      </Row>
+    </Container>
+  </BaseLayout>
+);
+
+export const getStaticProps = async ({ locale }) => ({
+  props: {
+    ...(await serverSideTranslations(locale, ['common'])),
+  },
+});
+
+export default About;
