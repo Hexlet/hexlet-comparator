6a3c0872d7b53a0a9346fd769e027980aa19237d
Vasiliy Boldurean <64642760+boldurean@users.noreply.github.com>
2021-08-03 10:35:47 +0100

    add layout spacing (#42)
    * add layout spacing

--- components/layouts/BaseLayout.jsx
+++ components/layouts/BaseLayout.jsx
@@ -11,7 +11,7 @@ import {
 const CustomNavbar = () => {
   const { t } = useTranslation('common');
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar className="flex-shrink-0" bg="light" expand="lg">
       <Container>
         <Navbar.Brand href="/">{t('navbar.brand')}</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -31,7 +31,7 @@ const CustomNavbar = () => {
 };
 
 const CustomFooter = () => (
-  <footer className="bg-light border-top mt-auto py-5">
+  <footer className="bg-light border-top mt-auto py-5 flex-shrink-0">
     <div className="container-xl">
       <div className="row justify-content-lg-around">
         <div className="col-sm-6 col-md-4 col-lg-auto">
@@ -98,11 +98,13 @@ const BaseLayout = (props) => {
         <title>{t('title')}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <CustomNavbar />
-      <main className="container my-5">
-        {children}
-      </main>
-      <CustomFooter />
+      <div className="min-vh-100 d-flex flex-column">
+        <CustomNavbar />
+        <main className="container my-5 flex-grow-1">
+          {children}
+        </main>
+        <CustomFooter />
+      </div>
     </>
   );
 };
