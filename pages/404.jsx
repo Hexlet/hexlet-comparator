75c4a39761aaa94eae4615e7de70ce7f3ea4dd68
Evgeniy <Evgst121@mail.ru>
2022-02-15 21:51:00 +0300

    [#25] Added custom error pages 404 and 500

--- pages/404.jsx
+++ pages/404.jsx
@@ -3,32 +3,36 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { Container, Row } from 'react-bootstrap';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useTranslation } from 'next-i18next';
 import BaseLayout from 'components/layouts/BaseLayout.jsx';
 import Head from 'next/head';
 import assetsRoutes from '../lib/assetsRoutes';
 
-const Custom404 = () => (
-  <BaseLayout>
-    <Head>
-      <title>Страница не найдена</title>
-    </Head>
-    <Container>
-      <Row>
-        <div className="container-sm text-center">
-          <Image layout="fixed" alt="page404" width="300" height="300" src={assetsRoutes.fallbackPath} />
-          <h1 className="h4 text-center">Ой, похоже, такой страницы нет!</h1>
-          <p className="text-center">
-            Но вы можете
-            {' '}
-            <Link href="/">
-              <a className="align-middle">перейти на главную</a>
-            </Link>
-          </p>
-        </div>
-      </Row>
-    </Container>
-  </BaseLayout>
-);
+const Custom404 = () => {
+  const { t } = useTranslation('common');
+  return (
+    <BaseLayout>
+      <Head>
+        <title>{ t('titles.title_error_404') }</title>
+      </Head>
+      <Container>
+        <Row>
+          <div className="container-sm text-center">
+            <Image layout="fixed" alt="page404" width="300" height="300" src={assetsRoutes.fallbackPath} />
+            <h1 className="h4 text-center">{ t('error.404.title') }</h1>
+            <p className="text-center">
+              { t('error.404.message') }
+              {' '}
+              <Link href="/">
+                <a className="align-middle">{ t('error.404.link') }</a>
+              </Link>
+            </p>
+          </div>
+        </Row>
+      </Container>
+    </BaseLayout>
+  );
+};
 
 export const getStaticProps = async ({ locale }) => ({
   props: {
