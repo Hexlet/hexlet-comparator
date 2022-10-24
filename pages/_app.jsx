ff9c6fbac6c5b7441ef6c6f2c4dfcc9fc977727f
askor26 <askorutin26@gmail.com>
2022-10-24 18:32:42 +0300

    make redirection to https

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,15 +1,31 @@
 /* eslint-disable react/jsx-props-no-spreading */
 // @ts-check
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { appWithTranslation } from 'next-i18next';
 import { DefaultSeo } from 'next-seo';
-
+import { useRouter } from 'next/router';
 import SEO from '../next-seo.json';
 
 import '../styles/app.scss';
 
 const HexletComparator = ({ Component, pageProps }) => {
+  const router = useRouter();
+  const { asPath } = useRouter();
+  useEffect(() => {
+    const origin = typeof window !== 'undefined' && window.location.origin
+      ? window.location.origin
+      : '';
+
+    const URL = `${origin}${asPath}`;
+    const parts = URL.split('//');
+    const protocol = parts[0];
+    const rest = parts[1];
+    if (protocol === 'http:') {
+      const newURL = `https://${rest}`;
+      router.push(newURL);
+    }
+  });
   const component = (
     <>
       <DefaultSeo {...SEO} />
