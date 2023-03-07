e8f34d8c6ca49fb006777b34bbd6a38f207d75f4
Evgeniy Sedov <sedoff-online@yandex.ru>
2023-03-07 15:18:16 +0700

    adds NEXT_PUBLIC_BASE_URL on next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 
 const { i18n } = require('./next-i18next.config.js');
 
+const NEXT_PUBLIC_BASE_URL = (process.env.NODE_ENV === 'production') ? "https://schools.hexlet.io" : 'http://localhost:3000';
+
 module.exports = {
   i18n,
   reactStrictMode: true,
@@ -13,5 +15,8 @@ module.exports = {
       },
     );
     return config;
+  },
+  env: {
+    NEXT_PUBLIC_BASE_URL,
   },
 };
